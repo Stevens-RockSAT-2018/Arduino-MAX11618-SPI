4364473e415844f3b81b73b275213dcacd3084c7
LegoMaster616 <andrew.afflitto@gmail.com>
2018-03-28 15:27:03 -0400

    add channel mode function

--- Arduino-MAX11618-SPI/Arduino-MAX11618-SPI.ino
+++ Arduino-MAX11618-SPI/Arduino-MAX11618-SPI.ino
@@ -1,6 +1,11 @@
 #include <SPI.h>
 #define CS_PIN 2
 
+#define SETUP_FLAG     10000000
+#define SCAN_MODE_NONE 00000110
+#define SCAN_MODE_0_N  00000000
+#define SCAN_MODE_N_4  00000100
+
 void setup() {
   pinMode(CS_PIN, OUTPUT);
   digitalWrite(CS_PIN, HIGH);
@@ -10,11 +15,11 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(captureValue());
+  Serial.println(readChannel0());
   delay(5);
 }
 
-unsigned int captureValue()
+unsigned int readChannel0()
 {
   digitalWrite(CS_PIN, LOW);
   byte msb = SPI.transfer(B10000110);
@@ -23,5 +28,19 @@ unsigned int captureValue()
   delayMicroseconds(20);
   digitalWrite(CS_PIN, HIGH);
   return (msb<<6 | lsb>>2);
+}
+
+unsigned int readMode(byte channel, byte scanMode)
+{
+  byte regData = SETUP_FLAG | channel << 3 | scanMode;
+  digitalWrite(CS_PIN, LOW);
+  byte msb = SPI.transfer(regData);
+  delayMicroseconds(20);
+  byte lsb = SPI.transfer(0x00);
+  delayMicroseconds(20);
+  digitalWrite(CS_PIN, HIGH);
+  return (msb<<6 | lsb>>2);
+
+  
 }
 
