5d8374385a9aa23ebfcf3efa1fedca8db2585558
LegoMaster616 <andrew.afflitto@gmail.com>
2018-04-10 21:03:46 -0400

    add arguement to specify chip select pin (allows multiple ADCs)

--- Arduino-MAX11618-SPI/Arduino-MAX11618-SPI.ino
+++ Arduino-MAX11618-SPI/Arduino-MAX11618-SPI.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
-#define CS_PIN 2
+#define CS_PIN_1 2
+#define CS_PIN_2 3
 
 #define SETUP_FLAG     B10000000
 #define SCAN_MODE_NONE B00000110
@@ -7,8 +8,10 @@
 #define SCAN_MODE_N_4  B00000100
 
 void setup() {
-  pinMode(CS_PIN, OUTPUT);
-  digitalWrite(CS_PIN, HIGH);
+  pinMode(CS_PIN_1, OUTPUT);
+  digitalWrite(CS_PIN_1, HIGH);
+  pinMode(CS_PIN_2, OUTPUT);
+  digitalWrite(CS_PIN_2, HIGH);
   Serial.begin(250000);
   SPI.begin();
   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
@@ -16,7 +19,14 @@ void setup() {
 
 void loop() {
   //Serial.println(readChannel0());
-  uint32_t ch0and1 = readChannel0and1();
+  uint32_t ch0and1 = readChannel0and1(CS_PIN_1);
+  Serial.print(ch0and1 >> 16);
+  Serial.print(" ");
+  Serial.print(ch0and1 & 0x03FF); //least sig 10 bits
+  Serial.print(" ");
+  //delay(5);
+  //2nd module
+  ch0and1 = readChannel0and1(CS_PIN_2);
   Serial.print(ch0and1 >> 16);
   Serial.print(" ");
   Serial.println(ch0and1 & 0x03FF); //least sig 10 bits
@@ -25,33 +35,36 @@ void loop() {
 
 unsigned int readChannel0()
 {
-  digitalWrite(CS_PIN, LOW);
+  digitalWrite(CS_PIN_1, LOW);
   byte msb = SPI.transfer(B10000110);
   delayMicroseconds(20);
   byte lsb = SPI.transfer(0x00);
   delayMicroseconds(20);
-  digitalWrite(CS_PIN, HIGH);
+  digitalWrite(CS_PIN_1, HIGH);
   return (msb<<6 | lsb>>2);
 }
 
-uint32_t readChannel0and1()
+uint32_t readChannel0and1(int csPin)
 {
   uint32_t data;
   uint16_t regData = SETUP_FLAG | 1 << 3 | SCAN_MODE_0_N;
-  digitalWrite(CS_PIN, LOW);
-  delayMicroseconds(20);
+  digitalWrite(csPin, LOW);
+  //delayMicroseconds(20);
+//  SPI.transfer(regData);
+  //delayMicroseconds(20);
+
   uint16_t msb = SPI.transfer(regData);
-  delayMicroseconds(20);
+  //delayMicroseconds(20);
   uint16_t lsb = SPI.transfer(0x00);
   data = (msb<<6 | lsb>>2);
   data <<= 16;
-  
+  //delayMicroseconds(20);
   msb = SPI.transfer(0x00);
-  delayMicroseconds(20);
+  //delayMicroseconds(20);
   lsb = SPI.transfer(0x00);
   data |= (msb<<6 | lsb>>2);
-  delayMicroseconds(20);
-  digitalWrite(CS_PIN, HIGH);
+  //delayMicroseconds(20);
+  digitalWrite(csPin, HIGH);
   
   return data;
 }
@@ -59,12 +72,12 @@ uint32_t readChannel0and1()
 unsigned int readMode(byte channel, byte scanMode)
 {
   byte regData = SETUP_FLAG | channel << 3 | scanMode;
-  digitalWrite(CS_PIN, LOW);
+  digitalWrite(CS_PIN_1, LOW);
   byte msb = SPI.transfer(regData);
   delayMicroseconds(20);
   byte lsb = SPI.transfer(0x00);
   delayMicroseconds(20);
-  digitalWrite(CS_PIN, HIGH);
+  digitalWrite(CS_PIN_1, HIGH);
   return (msb<<6 | lsb>>2);
 
   
